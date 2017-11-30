09923b0c980cad5388de64988a29ff0321479762
Wendorff <solqozero@gmail.com>
2017-11-30 16:31:00 -0200

    wenn@update

---
+++ src/TADFila.java
@@ -0,0 +1,66 @@
+
+public class TADFila {
+	
+	protected Nodo primeiro;
+	protected Nodo ultimo;
+	
+	public TADFila() {
+		this.primeiro = null;
+		this.ultimo = null;
+	}
+	
+	public void enqueue(Atendimento dado) {
+		Nodo novoNodo = new Nodo(dado);
+		if (isEmpty()) {
+			primeiro = novoNodo;
+			ultimo = novoNodo;
+		} else {
+			ultimo.proximo = novoNodo;
+			novoNodo.anterior = ultimo;
+			ultimo = novoNodo;
+		}
+	}
+	
+	public Atendimento dequeue() {
+		if (isEmpty()) {
+			return null;
+		} else {
+			Nodo aux = primeiro;
+			primeiro = primeiro.proximo;
+			primeiro.anterior = null;
+			return aux.dado;
+		}
+	}
+	
+	public Atendimento head() {
+		if (!isEmpty()) {
+			return primeiro.dado;
+		}
+		return null;
+	}
+	
+	public int size() {
+		int count=0;
+		Nodo aux = primeiro;
+		while (aux!=null) {
+			aux = aux.proximo;
+			count++;
+		}
+		return count;
+	}
+
+	public boolean isEmpty(){
+		if (primeiro==null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public void clear() {
+		while (!isEmpty()) {
+			dequeue();
+		}
+	}
+	
+}
