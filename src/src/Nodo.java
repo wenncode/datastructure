339e331fb17a585ba61d87fdbb0b01a714852b1a
Wendorff <solqozero@gmail.com>
2017-12-04 00:20:51 -0200

    wenn@commit

---
+++ src/src/Nodo.java
@@ -0,0 +1,15 @@
+
+public class Nodo {
+
+	protected Atendimento dado;	
+	protected Nodo proximo;
+	protected Nodo anterior;
+	
+	public Nodo(Atendimento dado){
+		this.dado = dado;
+		this.proximo = null;
+		this.anterior = null;
+	}
+	
+	
+}
